6bf0dc553744cccc67b3c6009be5710900a56438
Miguel Palhas <mpalhas@gmail.com>
2023-11-17 07:29:40 +0000

    code review

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/posts', title: 'Posts' },
+  { href: '/talks', title: 'Talks' },
+  { href: '/oss', title: 'Open Source' },
+]
+
+export default headerNavLinks
