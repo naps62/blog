97b6b66e5ffdcb184c0b4ffe1ce412e57fcdeb17
Miguel Palhas <mpalhas@gmail.com>
2023-11-20 12:56:46 +0000

    Using goatcounter

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import 'pliny/search/algolia.css'
 
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
@@ -11,6 +10,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { GCScript } from 'next-goatcounter'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -76,7 +76,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
               <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
@@ -88,6 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </SectionContainer>
         </ThemeProviders>
         <VercelAnalytics />
+        <GCScript siteUrl={'https://naps62.goatcounter.com/count'} />
       </body>
     </html>
   )
