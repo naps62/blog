718e860d03ec23fa099c15bb64a33f5436eda623
Miguel Palhas <mpalhas@gmail.com>
2025-07-06 19:04:44 +0100

    wip

--- src/routes/posts/$slug.tsx
+++ src/routes/posts/$slug.tsx
@@ -18,14 +18,14 @@ export const Route = createFileRoute("/posts/$slug")({
     ];
 
     console.log(frontmatter);
-    if (frontmatter.banner) {
-      const absoluteImageUrl = new URL(frontmatter.banner, VITE_VERCEL_URL)
-        .href;
-      meta.push(
-        { property: "og:image", content: absoluteImageUrl },
-        { name: "twitter:image", content: absoluteImageUrl },
-      );
-    }
+    // if (frontmatter.banner) {
+    //   const absoluteImageUrl = new URL(frontmatter.banner, VITE_VERCEL_URL)
+    //     .href;
+    //   meta.push(
+    //     { property: "og:image", content: absoluteImageUrl },
+    //     { name: "twitter:image", content: absoluteImageUrl },
+    //   );
+    // }
 
     if (frontmatter.canonicalUrl) {
       meta.push({ property: "og:url", content: frontmatter.canonicalUrl });
