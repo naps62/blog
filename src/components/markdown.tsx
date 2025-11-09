3a70e8ecc6e1e0e4a46b1334833f14385e54b3a9
Miguel Palhas <mpalhas@gmail.com>
2025-11-09 19:02:04 +0000

    New post: Custom Doorbell App with Home Assistant

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -108,6 +108,18 @@ const components = {
       <video className="w-full max-w-[600px]" {...props} />
     </picture>
   ),
+  Youtube: ({ videoId }: { videoId: string }) => (
+    <div className="my-6 flex w-full justify-center">
+      <iframe
+        className="aspect-video w-full max-w-[800px] rounded-lg"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
+  ),
   Figure: ({
     src,
     alt,
